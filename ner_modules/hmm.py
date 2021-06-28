59c251e62f45fa80a5aa31c75e7cb020030603ff
hanielf <hanielxx@outlook.com>
2021-06-28 15:10:57 +0800

    Update original hmm

--- ner_modules/hmm.py
+++ ner_modules/hmm.py
@@ -11,16 +11,17 @@ import os
 from collections import Counter
 from collections import defaultdict
 #  from allennlp.data.dataset_readers.dataset_utils.span_utils import bioul_tags_to_spans
-from allennlp.data.dataset_readers.dataset_utils.span_utils import InvalidTagSequence
+# from allennlp.data.dataset_readers.dataset_utils.span_utils import InvalidTagSequence
 
 import logging
+
+
 def get_logger(log_path=None, streamhandler=True, filehandler=False):
     if streamhandler is False and filehandler is False:
         return None
 
     logger = logging.getLogger(__name__)
-    fmt_str = ('%(asctime)s.%(msecs)03d %(levelname)7s '
-               '[%(thread)d][%(process)d] %(message)s')
+    fmt_str = ('%(asctime)s.%(msecs)03d %(levelname)7s ' '[%(thread)d][%(process)d] %(message)s')
     fmt = logging.Formatter(fmt_str, datefmt='%H:%M:%S')
 
     fh = ch = None
@@ -60,7 +61,7 @@ class hmm_tagger:
 
         self.logger.info("Init HMM tagger with {}".format(conll_path))
 
-        assert os.path.exists(conll_path), "Dataset doesn't exists!" 
+        assert os.path.exists(conll_path), "Dataset doesn't exists!"
 
         self.dataset_path = conll_path
 
@@ -76,16 +77,18 @@ class hmm_tagger:
         self.logger.info("words number of dataset: {}, max_word_num: {}".format(len(data_set), max_word_num))
 
         self.logger.info("Encoding dataset ...")
-        self.data, self.label, self.word_set, self.word2idx, self.lab2idx = self.encode_dataset(self.data, self.label, max_word_num)
-        self.idx2lab = {v:k for k,v in self.lab2idx.items()}
+        self.data, self.label, self.word_set, self.word2idx, self.lab2idx = self.encode_dataset(
+            self.data, self.label, max_word_num)
+        self.idx2lab = {v: k for k, v in self.lab2idx.items()}
 
-        assert len(self.data)==len(self.label)
-        self.logger.info("Sample cnt: {}, vocab size: {}, label cnt: {}".format(len(self.data), len(self.word2idx), len(self.lab2idx)))
+        assert len(self.data) == len(self.label)
+        self.logger.info("Sample cnt: {}, vocab size: {}, label cnt: {}".format(len(self.data), len(self.word2idx),
+                                                                                len(self.lab2idx)))
         self.logger.info("Label to index map: {}".format(self.lab2idx))
 
         self.logger.info("Calculating initial hidden matrix ...")
         self.init_mat = self.get_init_matrix(self.label, self.lab2idx)
-        self.logger.info("Initial hidden prob: {}".format({self.idx2lab[k]:v for k,v in self.init_mat.items()}))
+        self.logger.info("Initial hidden prob: {}".format({self.idx2lab[k]: v for k, v in self.init_mat.items()}))
 
         self.logger.info("Calculating transition matrix ...")
         self.transition_mat = self.get_transition_matrix(self.label, self.lab2idx)
@@ -98,9 +101,9 @@ class hmm_tagger:
 
         # metrics
         #  self.tags_to_spans_function = bioul_tags_to_spans
-        self._true_positives = defaultdict(int) #: Dict[str, int]
-        self._false_positives = defaultdict(int) #: Dict[str, int]
-        self._false_negatives = defaultdict(int) #: Dict[str, int]
+        self._true_positives = defaultdict(int)  #: Dict[str, int]
+        self._false_positives = defaultdict(int)  #: Dict[str, int]
+        self._false_negatives = defaultdict(int)  #: Dict[str, int]
 
         self.logger.info("Completed the training of HMM tagger")
 
@@ -123,20 +126,20 @@ class hmm_tagger:
         l_data = []
         l_lab = []
         for line in raw_data:
-            if len(line)==0:
-                if len(l_data)>0:
-                    assert len(l_data)==len(l_lab), "Length of data and label must be same"
+            if len(line) == 0:
+                if len(l_data) > 0:
+                    assert len(l_data) == len(l_lab), "Length of data and label must be same"
                     data.append(l_data)
                     label.append(l_lab)
                     l_data = []
                     l_lab = []
                     continue
             else:
-                if line[0]=='-DOCSTART-' or len(line)!=4:
+                if line[0] == '-DOCSTART-' or len(line) != 4:
                     continue
                 l_data.append(line[0].lower())
                 l_lab.append(line[-1])
-        
+
         return data, label
 
     def encode_dataset(self, data, label, max_word_num=15000):
@@ -149,19 +152,19 @@ class hmm_tagger:
             word2idx: map of word to index
             lab2idx: map of label to index
         '''
-        assert len(data)==len(label), "Length of data and label must be same"
+        assert len(data) == len(label), "Length of data and label must be same"
 
         all_words = [x for l in data for x in l]
         all_lab = [x for l in label for x in l]
 
-        lab2idx = {x:idx for idx, x in enumerate(set(all_lab))}
+        lab2idx = {x: idx for idx, x in enumerate(set(all_lab))}
 
         # word_freq: [('the', 1143), ('and', 966), ('to', 762), ('of', 669), ('i', 631)]
         word_freq = Counter(all_words).most_common(max_word_num)
         word_set = [x[0] for x in word_freq]
 
-        word2idx = {x:idx+1 for idx, x in enumerate(word_set)}
-        word2idx['unk']=0
+        word2idx = {x: idx + 1 for idx, x in enumerate(word_set)}
+        word2idx['unk'] = 0
 
         for i in range(len(data)):
             for j in range(len(data[i])):
@@ -171,8 +174,8 @@ class hmm_tagger:
                 if w not in word_set:
                     w = 'unk'
 
-                data[i][j]=word2idx[w]
-                label[i][j]=lab2idx[lab]
+                data[i][j] = word2idx[w]
+                label[i][j] = lab2idx[lab]
 
         return data, label, word_set, word2idx, lab2idx
 
@@ -191,14 +194,14 @@ class hmm_tagger:
         A = np.zeros((lab_len, lab_len))
 
         for sample in label_data:
-            for i in range(len(sample)-1):
-                A[sample[i]][sample[i+1]]+=1
+            for i in range(len(sample) - 1):
+                A[sample[i]][sample[i + 1]] += 1
 
         max_a = np.max(A, axis=-1, keepdims=True)
-        exp_a = np.exp(A-max_a)
+        exp_a = np.exp(A - max_a)
 
         sum_a = np.sum(exp_a, axis=-1, keepdims=True)
-        A = exp_a/sum_a
+        A = exp_a / sum_a
 
         return A
 
@@ -231,11 +234,11 @@ class hmm_tagger:
                 B[hidden[j]][sentense[j]] += 1
 
         max_b = np.max(B, axis=-1, keepdims=True)
-        exp_b = np.exp(B-max_b)
+        exp_b = np.exp(B - max_b)
 
         sum_b = np.sum(exp_b, axis=-1, keepdims=True)
-        B = exp_b/sum_b
-        
+        B = exp_b / sum_b
+
         return B
 
     def get_init_matrix(self, label, lab2idx):
@@ -248,16 +251,16 @@ class hmm_tagger:
         return:
             lab_freq: dict of label to frequency, {lab_idx: freq}
         '''
-        lab_freq = {idx:0 for k, idx in lab2idx.items()}
+        lab_freq = {idx: 0 for k, idx in lab2idx.items()}
         all_cnt = 0
 
         for line in label:
             for lab in line:
-                lab_freq[lab] = lab_freq.get(lab, 0)+1
+                lab_freq[lab] = lab_freq.get(lab, 0) + 1
                 all_cnt += 1
 
-        for k,v in lab_freq.items():
-            lab_freq[k] = lab_freq.get(k)/all_cnt
+        for k, v in lab_freq.items():
+            lab_freq[k] = lab_freq.get(k) / all_cnt
 
         return lab_freq
 
@@ -273,7 +276,7 @@ class hmm_tagger:
             init_mat: probability of hidden state 
             logger: to log info
         '''
-        assert os.path.exists(val_path),"Validation dataset does not exists!" 
+        assert os.path.exists(val_path), "Validation dataset does not exists!"
 
         self.val_data, self.val_label = self.read_data(val_path)
 
@@ -281,7 +284,8 @@ class hmm_tagger:
         self.logger.info("Evaluate sample cnt: {}".format(len(self.val_data)))
 
         for line in self.val_data:
-            predicted_tag = self.predict(line, self.word2idx, self.idx2lab, self.transition_mat, self.observation_mat, self.init_mat)
+            predicted_tag = self.predict(line, self.word2idx, self.idx2lab, self.transition_mat, self.observation_mat,
+                                         self.init_mat)
             self.val_res.append(predicted_tag)
 
         return self.val_label, self.val_res
@@ -298,8 +302,8 @@ class hmm_tagger:
             init_mat: probability of hidden state 
             logger: to log info
         '''
-        assert len(data)>0, "Data to be predicted must be non-empty!" 
-        
+        assert len(data) > 0, "Data to be predicted must be non-empty!"
+
         num_hidden = len(idx2lab)
         num_word = len(data)
 
@@ -310,17 +314,20 @@ class hmm_tagger:
 
         # init with first word
         for i in range(num_hidden):
-            viterbi_mat[i][0] = init_mat[i]*observation_mat[i][sentense_idx[0]]
+            viterbi_mat[i][0] = init_mat[i] * observation_mat[i][sentense_idx[0]]
 
         # i indicates cur position
-        for i in range(1,num_word):
+        for i in range(1, num_word):
             # j indicates cur possible hidden state loc
             for j in range(num_hidden):
                 # x indicates last possible hidden state loc
-                trans_observe_prob = [ viterbi_mat[x][i-1]*transition_mat[x][j]*observation_mat[j][sentense_idx[i]] for x in range(num_hidden)]
+                trans_observe_prob = [
+                    viterbi_mat[x][i - 1] * transition_mat[x][j] * observation_mat[j][sentense_idx[i]]
+                    for x in range(num_hidden)
+                ]
                 viterbi_mat[j][i] = max(trans_observe_prob)
 
-        res_lab = [np.argmax(viterbi_mat[:,i]) for i in range(num_word)]
+        res_lab = [np.argmax(viterbi_mat[:, i]) for i in range(num_word)]
         res = [idx2lab[x] for x in res_lab]
         return res
 
@@ -350,12 +357,12 @@ class hmm_tagger:
 
             #  predicted_spans = self.tags_to_spans_function(i_pre)
             #  gold_spans = self.tags_to_spans_function(i_lab)
-            self.logger.info("label sequence:{}".format(i_lab))
-            self.logger.info("predict sequence:{}".format(i_pre))
+            #  self.logger.info("label sequence:{}".format(i_lab))
+            #  self.logger.info("predict sequence:{}".format(i_pre))
             predicted_spans = self.bioul_tags_to_spans(i_pre)
-            self.logger.info("predicted_span:{}".format(predicted_spans))
+            #  self.logger.info("predicted_span:{}".format(predicted_spans))
             gold_spans = self.bioul_tags_to_spans(i_lab)
-            self.logger.info("gold_spans:{}".format(gold_spans))
+            #  self.logger.info("gold_spans:{}".format(gold_spans))
 
             for span in predicted_spans:
                 if span in gold_spans:
@@ -373,9 +380,8 @@ class hmm_tagger:
         all_tags.update(self._false_negatives.keys())
         all_metrics = {}
         for tag in all_tags:
-            precision, recall, f1_measure = self._compute_metrics(
-                self._true_positives[tag], self._false_positives[tag], self._false_negatives[tag]
-            )
+            precision, recall, f1_measure = self._compute_metrics(self._true_positives[tag], self._false_positives[tag],
+                                                                  self._false_negatives[tag])
             precision_key = "precision" + "-" + tag
             recall_key = "recall" + "-" + tag
             f1_key = "f1-measure" + "-" + tag
@@ -392,9 +398,29 @@ class hmm_tagger:
         all_metrics["precision-overall"] = precision
         all_metrics["recall-overall"] = recall
         all_metrics["f1-measure-overall"] = f1_measure
+
+        all_metrics['accuracy'] = self._compuate_acc(label, predict)
         if reset:
             self.reset_metrics()
         return all_metrics
+
+    def _compuate_acc(label, predict):
+        '''
+
+        paras:
+            label: 2dList(str) e.g. [["B-PER", "L-PER", "O"]]
+            predict: 2dList(str) e.g. [["B-PER", "L-PER", "O"]]
+        '''
+        true = 0
+
+        org = [item for sublist in org_ent for item in sublist]
+        pred = [item for sublist in pred_ent for item in sublist]
+
+        for t, r in zip(org, pred):
+            if t == r:
+                true += 1
+        return true/len(org)
+
 
     @staticmethod
     def _compute_metrics(true_positives: int, false_positives: int, false_negatives: int):
@@ -408,7 +434,7 @@ class hmm_tagger:
         self._false_positives = defaultdict(int)
         self._false_negatives = defaultdict(int)
 
-    def bioul_tags_to_spans(self, tag_sequence, classes_to_ignore = None):
+    def bioul_tags_to_spans(self, tag_sequence, classes_to_ignore=None):
         """
         Given a sequence corresponding to BIOUL tags, extracts spans.
         Spans are inclusive and can be of zero length, representing a single word span.
@@ -453,18 +479,15 @@ class hmm_tagger:
                 if span_flag:
                     spans.append((label.partition("-")[2], (start, index)))
             #  else:
-                #  if label != "O":
-                    #  print(label)
-                    #  raise InvalidTagSequence(tag_sequence)
-                    #  continue
+            #  if label != "O":
+            #  print(label)
+            #  raise InvalidTagSequence(tag_sequence)
+            #  continue
             index += 1
         return [span for span in spans if span[0] not in classes_to_ignore]
 
 
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     train_path = './data/eng.train'
     val_path = './data/eng.testa'
     test_path = './data/eng.testb'
@@ -478,7 +501,7 @@ if __name__=='__main__':
     max_word_num = None
 
     # train phase
-    tagger = hmm_tagger(conll_path=train_path, max_word_num=max_word_num, logger=logger)    
+    tagger = hmm_tagger(conll_path=train_path, max_word_num=max_word_num, logger=logger)
 
     # evaluate phase
     logger.info("Evaluate with: {}".format(os.path.basename(val_path)))
@@ -511,4 +534,3 @@ if __name__=='__main__':
             for j in range(len(test_lab[i])):
                 fout.write("{}\t{}\n".format(test_lab[i][j], test_res[i][j]))
             fout.write("\n")
-
